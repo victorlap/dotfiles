a134ed65b2014203334f2754bec1cd5f31451209
Victor Lap <victorlap@outlook.com>
2020-03-12 12:00:55 +0100

    Fix duplicate composer + yarn install

--- install.sh
+++ install.sh
@@ -17,18 +17,11 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
 # Install global Composer packages
 /usr/local/bin/composer global require hirak/prestissimo laravel/installer laravel/valet
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
-
-# Install global NPM packages
-npm install --global yarn
 
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
